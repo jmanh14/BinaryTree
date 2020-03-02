b3ade03ec79634b157dc0634185caa0b67f032db
Justin Haldeman <jmanh14@gmail.com>
2020-03-02 07:07:54 -0600

    Added add method + started search method

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        public Node root;
+        
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void Add( Node node)
+        {
+            Node tempNode = new Node(node.data);
+            if (root == null)
+            {
+                root = tempNode;
+                return;
+            }
+            else
+            {
+                Node currentNode = root;
+                Node parent;
+                while (true)
+                {
+                    parent = currentNode;
+                    if (node.data < currentNode.data)
+                    {
+                        currentNode = currentNode.leftNode;
+                        if (currentNode == null)
+                        {
+                            parent.leftNode = node;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        currentNode = currentNode.rightNode;
+                        if (currentNode == null)
+                        {
+                            parent.rightNode = node;
+                            return;
+                        }
+                    }
+                }
+            }
+            
+        }
+        public bool Search(Node node)
+        {
+            if (root == null)
+                return false;
+            if (root.data == node.data)
+                return true;
+
+            bool isInLeft = Search(node.leftNode);
+            if (isInLeft)
+                return true;
+
+            bool isInRight = Search(node.rightNode);
+
+            return isInRight;
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            Node nodeOne = new Node(5);
+            Node nodeTwo = new Node(6);
+            Node nodeThree = new Node(4);
+            binaryTree.Add(nodeOne);
+            binaryTree.Add(nodeTwo);
+            binaryTree.Add(nodeThree);
+            Console.WriteLine(binaryTree.Search(nodeTwo));
+            Console.ReadLine();
+        }
+    }
+}
